33798c0cc64ff4ccf091cd1127cba602409f7138
Sean Altemose <sean@seanaltemose.com>
2019-10-02 19:47:01 +0000

    Automatically backed up by Learn

--- app/models/github_repo.rb
+++ app/models/github_repo.rb
@@ -1,2 +1,8 @@
-class GithubRepo < ApplicationRecord
+class GithubRepo
+  attr_reader :name, :url
+
+    def initialize(hash)
+      @name = hash["name"]
+      @url = hash["html_url"]
+    end 
 end
