1da238a25ab37221efd7c82db3bd01cb7174c3c1
Sean Altemose <sean@seanaltemose.com>
2019-10-02 20:23:59 +0000

    Automatically backed up by Learn

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -27,5 +27,9 @@ class GithubService
     "https://api.github.com/user/repos", {}, {'Authorization' => "token #{self.access_token}", 'Accept' => 'application/json'}
     repos_array = JSON.parse(response.body)
     repos_array.map{|repo| GithubRepo.new(repo)}
+  end
+
+  def create_repo(name)
+    Faraday.post "https://api.github.com/user/repos", {name: name}.to_json, {'Authorization' => "token #{self.access_token}", 'Accept' => 'application/json'}
   end 
 end
