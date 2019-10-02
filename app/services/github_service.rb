9376744eacb269ab971a7dc57c2a5bce3812fa2f
Sean Altemose <sean@seanaltemose.com>
2019-10-02 20:17:09 +0000

    Automatically backed up by Learn

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -1,2 +1,13 @@
 class GithubService
-end 
+
+  attr_reader :access_token
+
+  def initialize(access_hash = nil)
+    @access_token = access_hash["access_token"] if access_hash
+  end
+
+  def authenticate!(client_id, client_secret, code)
+    response = Faraday.post
+    "https://github.com/login/oauth/access_token",
+    {client_id: client_id, client_secret: client_secret, code: code}
+end
