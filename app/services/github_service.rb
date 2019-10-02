3aaf51460bad2ecf967cb1b0f1b209535c7188fa
Sean Altemose <sean@seanaltemose.com>
2019-10-02 20:19:57 +0000

    Automatically backed up by Learn

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -9,5 +9,13 @@ class GithubService
   def authenticate!(client_id, client_secret, code)
     response = Faraday.post
     "https://github.com/login/oauth/access_token",
-    {client_id: client_id, client_secret: client_secret, code: code}
+    {client_id: client_id, client_secret: client_secret, code: code},
+    {'Accept' => 'application/json'}
+    access_hash = JSON.parse(response.body)
+    @access_token = access_hash["access_token"]
+  end 
+
+  def get_username
+    user_response = Faraday.get
+    "https://api.github.com/user", {}, {'Authorization' => "token #{self.access_token}", 'Accept' => 'application/json'}
 end
